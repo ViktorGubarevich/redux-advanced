facf6855a0ae3592a212f684b32e20052ef052d7
ViktorGubarevich <gubarew.v@gmail.com>
2023-03-21 13:47:00 +0300

    Git init project.

---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+import styles from "./Card.module.css";
+
+const Card = ({ className, children }) => {
+  return (
+    <section className={`${styles.card} ${className ? className : ""}`}>
+      {children}
+    </section>
+  );
+};
+
+export default Card;
